c8b1b2ed349d37985b87d089583d476dfe770991
Jack Muir <jmuirbusiness@gmail.com>
2024-01-06 18:31:03 +0000

    API developed - database, endpoints, controllers, dummy data

---
+++ API/Data/DbInitializer.cs
@@ -0,0 +1,218 @@
+using API.Entities;
+
+namespace API.Data;
+
+public static class DbInitializer
+{
+    public static void Initialize(StoreContext context)
+    {
+        if (context.Products.Any()) return;
+
+        var products = new List<Product>
+            {
+                new Product
+                {
+                    Name = "Angular Speedster Board 2000",
+                    Description =
+                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 20000,
+                    Image = "/images/products/sb-ang1.png",
+                    Brand = "Angular",
+                    Category = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Green Angular Board 3000",
+                    Description = "Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.",
+                    Price = 15000,
+                    Image = "/images/products/sb-ang2.png",
+                    Brand = "Angular",
+                    Category = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Core Board Speed Rush 3",
+                    Description =
+                        "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+                    Price = 18000,
+                    Image = "/images/products/sb-core1.png",
+                    Brand = "NetCore",
+                    Category = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Net Core Super Board",
+                    Description =
+                        "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.",
+                    Price = 30000,
+                    Image = "/images/products/sb-core2.png",
+                    Brand = "NetCore",
+                    Category = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "React Board Super Whizzy Fast",
+                    Description =
+                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 25000,
+                    Image = "/images/products/sb-react1.png",
+                    Brand = "React",
+                    Category = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Categoryscript Entry Board",
+                    Description =
+                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 12000,
+                    Image = "/images/products/sb-ts1.png",
+                    Brand = "CategoryScript",
+                    Category = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Core Blue Hat",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1000,
+                    Image = "/images/products/hat-core1.png",
+                    Brand = "NetCore",
+                    Category = "Hats",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Green React Woolen Hat",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 8000,
+                    Image = "/images/products/hat-react1.png",
+                    Brand = "React",
+                    Category = "Hats",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Purple React Woolen Hat",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1500,
+                    Image = "/images/products/hat-react2.png",
+                    Brand = "React",
+                    Category = "Hats",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Blue Code Gloves",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1800,
+                    Image = "/images/products/glove-code1.png",
+                    Brand = "VS Code",
+                    Category = "Gloves",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Green Code Gloves",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1500,
+                    Image = "/images/products/glove-code2.png",
+                    Brand = "VS Code",
+                    Category = "Gloves",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Purple React Gloves",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1600,
+                    Image = "/images/products/glove-react1.png",
+                    Brand = "React",
+                    Category = "Gloves",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Green React Gloves",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1400,
+                    Image = "/images/products/glove-react2.png",
+                    Brand = "React",
+                    Category = "Gloves",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Redis Red Boots",
+                    Description =
+                        "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+                    Price = 25000,
+                    Image = "/images/products/boot-redis1.png",
+                    Brand = "Redis",
+                    Category = "Boots",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Core Red Boots",
+                    Description =
+                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 18999,
+                    Image = "/images/products/boot-core2.png",
+                    Brand = "Redis",
+                    Category = "Boots",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Core Purple Boots",
+                    Description =
+                        "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.",
+                    Price = 19999,
+                    Image = "/images/products/boot-core1.png",
+                    Brand = "NetCore",
+                    Category = "Boots",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Angular Purple Boots",
+                    Description = "Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.",
+                    Price = 15000,
+                    Image = "/images/products/boot-ang2.png",
+                    Brand = "Angular",
+                    Category = "Boots",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Angular Blue Boots",
+                    Description =
+                        "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+                    Price = 18000,
+                    Image = "/images/products/boot-ang1.png",
+                    Brand = "Angular",
+                    Category = "Boots",
+                    QuantityInStock = 100
+                },
+            };
+
+        foreach (var product in products)
+        {
+            context.Products.Add(product);
+        }
+
+        context.SaveChanges();
+    }
+}
