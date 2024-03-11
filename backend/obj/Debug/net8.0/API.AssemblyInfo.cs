08ebed8f7cc5e5b0b3269d71932d6c53d809f686
Jack Muir <jmuirbusiness@gmail.com>
2024-03-11 15:32:39 +0000

    bug fixes

--- backend/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19410fb857272dd72cb8640a3928479b9d7e2e9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35999d69a84fefed6685fe3c3eb13828a67e0950")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
